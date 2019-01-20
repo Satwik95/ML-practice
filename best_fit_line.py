8f2a6db908b929e3aaa5b4a40e50490884879306
Satwik <satwik.mishra1995@gmail.com>
2019-01-19 20:39:37 -0500

    tested assumption on randomly genearted data set

--- best_fit_line.py
+++ best_fit_line.py
@@ -2,12 +2,26 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('ggplot')
 
-X = np.array([2,3,4,5,6,7,8])
-Y = np.array([5,9,12,10,14,8,19])
+# X = np.array([2,3,4,5,6,7,8])
+# Y = np.array([5,9,12,10,14,8,19])
 
+def create_data(num, variance, step=2, correlation=False):
+    val = random.randint(-variance, variance)
+    Y = []
+    for i in range(num):
+        Y.append(random.randint(-variance, variance) + val)
+        if correlation:
+            if correlation=='pos':
+                val+=step
+            else:
+                val-=step
+    X = [i for i in range(len(Y))]
+    return np.array(X, dtype=np.float64), np.array(Y, dtype=np.float64)
+    
 def best_fit(X,Y):
     m = ((mean(X)*mean(Y))-(mean(X*Y)))/((mean(X)*mean(X))-(mean(X*X)))
     c = mean(Y)-(m*mean(X))
@@ -22,6 +36,7 @@ def r_sq(Y_orig, Y_new):
     sq_y_mean = sq_error(Y_orig, Y_mean)
     return 1 - (sq_y_line/sq_y_mean)
 
+X, Y = create_data(100, 20,2,correlation='neg')
 m, c = best_fit(X,Y)
 
 reg_line = [(m*x)+c for x in X]
